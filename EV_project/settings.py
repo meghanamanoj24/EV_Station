a3447d5fc6a8d4b842b3ca0669714c34166b26b3
meghanamanoj24 <156163063+meghanamanoj24@users.noreply.github.com>
2025-05-14 10:20:01 +0530

    Update settings.py

--- EV_project/settings.py
+++ EV_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-35#yl*1=^)ae@4u)(%gz!k&zyu#4txd8%7@0augkksl6c9w5hi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
